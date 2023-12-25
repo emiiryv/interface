d8925397fd0fe2c4616835b3d3366bface6bf854
emiiryv <111075935+emiiryv@users.noreply.github.com>
2023-12-25 20:57:10 +0300

    Initial commit

---
+++ src/AssecoPaymentSystem.java
@@ -0,0 +1,16 @@
+public class AssecoPaymentSystem implements PaymentProvider {
+    @Override
+    public boolean cancelCharge(int chargeId) {
+        // TODO Auto-generated method stub
+        return false;
+        }
+    @Override
+    public int charge(double totalPrice) {
+         // TODO Auto-generated method stub
+            return 0; 	}
+    @Override
+        public String loadInvoice(int chargeId) {
+        // TODO Auto-generated method stub
+     return null;
+    }
+}
